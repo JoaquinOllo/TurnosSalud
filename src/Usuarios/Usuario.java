a0e8998838fd61509c4ebb5d327429d0162613e8
marian369 <mardelvallerojas@gmail.com>
2024-10-28 18:12:34 -0300

    prueba

--- src/Usuarios/Usuario.java
+++ src/Usuarios/Usuario.java
@@ -10,4 +10,59 @@ public abstract class Usuario {
     private Usuario contacto;
     private String direccion;
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNroTelefono() {
+        return nroTelefono;
+    }
+
+    public void setNroTelefono(String nroTelefono) {
+        this.nroTelefono = nroTelefono;
+    }
+
+    public Usuario getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(Usuario contacto) {
+        this.contacto = contacto;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
