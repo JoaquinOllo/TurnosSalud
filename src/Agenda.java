533c741f04e18e37b5b6004d2cfb7635429df2ac
marian369 <mardelvallerojas@gmail.com>
2024-10-28 18:55:58 -0300

    agregue los filtros de Agenda

--- src/Agenda.java
+++ src/Agenda.java
@@ -4,22 +4,33 @@ import Usuarios.Profesional;
 
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Agenda<T extends Cita> extends ArrayList<T> {
 
     public boolean franjaDisponible(T turno) {
         Agenda<T> agendaFiltrada = this.filtrarPorProfesional(turno.getProfesional()).filtrarPorConsultorio(turno.getConsultorio());
         //completar
-        return agendaFiltrada.stream().anyMatch(t -> t.colisiona(turno));
+        return agendaFiltrada.stream().anyMatch(t -> t.colisiona(turno));//recorre el array de agenda filtrada
     }
 
     private Agenda<T> filtrarPorConsultorio(Consultorio consultorio) {
-        //completar
-        return this;
+        Agenda <T> turnosPorConsultorio=new Agenda<>();
+        for (T turno:this){
+            if(turno.getConsultorio().equals(consultorio)){
+                turnosPorConsultorio.add(turno);
+            }
+        }
+        return turnosPorConsultorio;
     }
 
     private Agenda<T> filtrarPorProfesional(Profesional profesional) {
-        //completar
-        return this;
+        Agenda<T> turnosPorProfesional=new Agenda<>();
+        for(T turno:this){
+            if(turno.getProfesional().equals(profesional)){
+                turnosPorProfesional.add(turno);
+            }
+        }
+        return turnosPorProfesional;
     }
 }
